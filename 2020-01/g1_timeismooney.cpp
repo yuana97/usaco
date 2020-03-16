0695808820a065a29eef7609a1aaf5e95ccdbc1a
Allen Yuan <alleyuan@amazon.com>
2020-03-15 18:00:45 -0700

    concise

--- 2020-01/g1_timeismooney.cpp
+++ 2020-01/g1_timeismooney.cpp
@@ -20,122 +20,73 @@ We do the above for t = 1,...,1000 and write the answer to the output file.
 
 */
 
-// include std libs
 #include <iostream>
 #include <vector>
 #include <algorithm>
-// use standard namespace to omit the std:: from standard functions
-// google 'std namespace'
 using namespace std;
 
-// initialize max values of N and T.
-// initialize means to declare a variable and assign it a value
-// google 'programming initialize'
+// initialize MAXN=max #cities, MAXT=max time
 const int MAXN = 1005;
 const int MAXT = 1005;
 
-// declare n=num cities, m=num roads, c=constant to be read in
-// declare means to give a variable a type and a name (so the computer reserves memory for it)
-// google programming 'declare'
+// declare n=#cities, m=#roads, c=travel constant
 long long n, m, c;
-// declare value where value[i] = money at city i
+// declare value[i]=money at city i
 long long value[MAXN];
-// initialize 2d dp array with 2 rows and MAXN columns
-// dp[t][x] represents the most money Bessie can have at the city x at time t
-// (where t is considered mod 2)
+// declare dp[t][x]=max money at city x at time t
 long long dp[2][MAXN];
-
-// declare vector of int pairs "edges" representing the edges of the graph
-// google 'cpp vector', 'cpp pair', 'graph edges'
+// declare vector of edges of graph
 vector<pair<int, int>> edges;
 
 // program entry point
 int main()
 {
-  // use standard input stream 'cin' to read from time.in
-  // google 'cpp standard streams'
+  // use cin to read from time.in
   freopen("time.in", "r", stdin);
-  // use standard output stream 'cout' to write to time.out
+  // use cout to write to time.out
   freopen("time.out", "w", stdout);
-  // assign n to the first token from the read stream
-  // m to the second token, c to the third token
+  // read in n, m, c
   cin >> n >> m >> c;
-  // fill the value array with money values
+  // initialize value[i]=money at city i
   for (int i = 1; i <= n; i++)
   {
-    // assign value[i] to the next token in the read stream
     cin >> value[i];
   }
   // declare a,b = endpoints of next edge
   int a, b;
+  // read in each edge and push it to edges
   for (int i = 0; i < m; i++)
   {
-    // assign a to first token from read stream, b to second token
     cin >> a >> b;
-    // push the pair (a, b) to edges.
-    // more detail: std::make_pair(a, b) returns a pair<int, int> 'x' with x.first=a, x.second=b.
-    // Then edges.push_back(x) makes x the last element in the vector.
     edges.push_back(make_pair(a, b));
   }
   // initialize max profit as 0
   long long max_profit = 0;
-  // initialize dp to be filled with the value -1
-  // dp[t][x] = -1 represents city x is unreachable by time t
-  // more detail: sizeof dp = number of bytes in the block of memory reserved for dp.
-  // dp = pointer to start of block of memory. std::memset(dp, -1, sizeof dp) writes
-  // -1 to (sizeof dp) number of bytes starting at dp. So it writes -1 to every byte of dp.
-  // -1 in binary is all ones, so memset(dp, -1, sizeof dp) fills the dp block of memory with
-  // all ones. When we interpret dp[t][x] as an integer, we get 4 bytes of -1 (111111111111 x4)
-  // which is the same as -1 as an integer.
-  // summary: fill dp memory block with all 1's. This is the same as filling the array dp with
-  // all -1's.
-  // why do this: style points + faster and less lines of code.
-  // google 'twos complement' and 'cpp memset'
+  // fill dp with -1 representing 'unreachable'
   memset(dp, -1, sizeof dp);
-  // at time 0 at city 1 the most money Bessie can earn is 0.
-  // remember dp[t][x] = max money at time t at city x
+  // base case: at time 0 at city 1 the max money is 0. all other cities unreachable at time 0.
   dp[0][1] = 0;
   for (int t = 1; t < MAXT; t++)
   {
-    // initialize parity p = t%2
-    // parity = whether number is even or odd
-    // p = 0 means number is even, p = 1 means number is odd
-    // this is a way to track 'current time' where p is current time
-    // and 1-p is the previous time (notice 1-0=1, 1-1=0 so 1-p is the 
-    // opposite of p)
+    // p = current time
     int p = t % 2;
-    // reset the values of dp[p] to -1. remember p = current time.
+    // reset the values of dp[p] to -1
     memset(dp[p], -1, sizeof dp[p]);
     // for each edge in edges
-    // more detail: auto means figure out what type e is based on edges.
-    // in this case edges is a vector<pair<int, int>> so auto becomes pair<int, int>.
-    // the & indicates to pass by reference. This means it passes a reference to the memory
-    // address so you're getting the original object. On the other hand for(auto e : edges) makes
-    // a copy of each edge e which makes it slower. Its faster to pass a reference to the original
-    // object than to copy the object and then pass a reference to the new object.
     for (auto &e : edges)
     {
-      // assign a,b to the endpoints of e
+      // a=source city
       a = e.first;
+      // b=endpoint city
       b = e.second;
-      // if at the previous time we traveled to city a
-      // remember dp[1-p][x] is the most money we could
-      // get at the previous time at city x
+      // if city a is reachable at previous time
       if (dp[1 - p][a] >= 0)
       {
-        // try traveling from a to b
-        // more detail: dp[p][b] = current max money at b, (dp[1-p][a] + value[b])
-        // equals max money traveling to a previously then coming to b.
-        // we update dp[p][b]=current max money at b with the new possible max
-        // dp[1-p][a] + value[b].
+        // update dp[p][b] with recurrence: see if walking to b thru a gives Bessie more money
         dp[p][b] = max(dp[p][b], dp[1 - p][a] + value[b]);
       }
     }
     // update max profit for each time t
-    // detail: dp[p][1] = max money for a trip ending at city 1. Bessie's trip has to
-    // end at 1 so this is the max money for a single trip. c*t*t is the travling cost
-    // so we update max with the net profit dp[p][1] - c*t*t. max profit is initialized as
-    // 0 so this value is never negative.
     max_profit = max(max_profit, dp[p][1] - c * t * t);
   }
   // write max profit to the output stream followed by a newline.
